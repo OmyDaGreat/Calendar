0aa3c287bf5623c367274efa1e33c1c30c5396fe
Malefic Marauder <ogupta4242@gmail.com>
2024-04-22 21:26:15 -0700

    Kind of working

--- docs/main.py
+++ docs/main.py
@@ -4,11 +4,11 @@ from festivals import IndianFestivals
 fest = IndianFestivals(2024)
 
 # Write output to a file instead of printing
-with open('output.txt', 'w') as f:
+with open('docs/output.txt', 'w') as f:
     f.write("Festivals in 2024:\n")
     f.write(fest.get_festivals_in_a_year())
-    f.write("\n================================================")
 '''
+    f.write("\n================================================")
     f.write("\nFestivals in February 2024:\n")
     f.write(fest.get_festivals_in_a_month(2))
     f.write("\n================================================")
